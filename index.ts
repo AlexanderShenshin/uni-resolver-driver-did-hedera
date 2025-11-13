75eccdd9cefcaec0af7e039ae36109b094f33a3c
Alexander Shenshin <93187809+AlexanderShenshin@users.noreply.github.com>
2025-11-13 13:49:43 +0300

    Push Hedera Universal Resolver driver
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { Elysia } from 'elysia'
+import { resolveDID } from '@hiero-did-sdk/resolver';
+
+const app = new Elysia()
+  .get('/', () => {
+    return new Response('Hello World', {status: 200});
+  })
+  .get('/1.0/identifiers/:identifier', async ({ params: { identifier } }: { params: { identifier: string } }) => {
+    console.log(`Resolving ${identifier}`)
+    try {
+      const { didDocument, didResolutionMetadata, didDocumentMetadata } = await resolveDID(identifier, 'application/ld+json;profile="https://w3id.org/did-resolution"');
+      if (didDocument) {
+        console.log(`Resolved ${didDocument.id}`);
+        return Response.json({
+          didDocument,
+          didResolutionMetadata,
+          didDocumentMetadata
+        }, {
+          headers: {
+              'Content-Type': 'application/json'
+          }
+        });
+      }
+      return new Response('Not Found', {status: 404});
+    } catch (e: any) {
+      console.log(`Failed to resolve ${identifier}: ${e.message}`)
+      return new Response(e.message, {status: 400});
+    }
+  })
+  .listen(Bun.env.PORT || 8080)
+
+console.log(`did:hedera resolver is running on port ${app.server?.port}`)
